4df96a11125a463b90731613160f94e6b684cd47
navrajveer <141467854+navrajveer@users.noreply.github.com>
2024-08-03 17:26:24 -0600

    firebase initialization and authentication

---
+++ newscentral/app/_utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZgPhvPzhfCGIeuufy57pL3SmF600FG2M",
+  authDomain: "newscentral-1234c.firebaseapp.com",
+  projectId: "newscentral-1234c",
+  storageBucket: "newscentral-1234c.appspot.com",
+  messagingSenderId: "879828794926",
+  appId: "1:879828794926:web:a0dbc8cdf6a128795129bf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
