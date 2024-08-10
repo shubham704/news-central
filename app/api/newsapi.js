88bd63a60ebdb56b2218d1a3a6c590c896c046b8
shubham704 <shubhamprasharr40@gmail.com>
2024-08-10 13:51:56 -0600

    modifying header.js, added custom css for header and modify newsapi.js to fetch posts in card on mainpage

--- app/api/newsapi.js
+++ app/api/newsapi.js
@@ -1,40 +1,48 @@
-"use client"
+"use client";
+import '../styles/style.css'
+// import '../styles/bootstrap.css'
 
 import { useState } from "react";
 
-export default function Articles(){
-  const [article, setArticle] = useState([]); 
+export default function Articles() {
+  const [articles, setArticles] = useState([]);
 
   const fetchArticles = async () => {
-    try{
-      const response = await fetch("https://newsapi.org/v2/everything?q=Apple&apiKey=22e63dc207ab4724a1ebe6a5857af966"
-
+    try {
+      const response = await fetch(
+        "https://newsapi.org/v2/everything?q=business&apiKey=22e63dc207ab4724a1ebe6a5857af966"
       );
       const articleData = await response.json();
-      return articleData.article;
-
-    }catch(err){
-      console.error("error fetching articles:",error);
+      return articleData.articles;
+    } catch (error) {
+      console.error("Error fetching articles:", error);
     }
   };
- 
-  
-  const loadArticles = async () =>{
-    const articles = await fetchArticles();
-    setArticle(articles || []);
-  }
+
+  const loadArticles = async () => {
+    const fetchedArticles = await fetchArticles();
+    setArticles(fetchedArticles || []);
+  };
 
   return (
     <div>
       <button onClick={loadArticles}>Load Articles</button>
-      <ul>
-        {article.map((article, index) => (
-          <li key={index}>
-            <h2>{article.title}</h2>
-            <p>{article.description}</p>
-          </li>
-          ))}
-          </ul>
-          </div>
-          );
+      <div>
+      <ul className="news-list">
+            {articles.map((article, index) => (
+                <li key={index} className="news-item">
+                    <div className="news-card">
+                        <img src={article.urlToImage} alt={article.title} className="news-image" />
+                        <div className="news-content">
+                            <h2 className="news-title">{article.title}</h2>
+                            <p className="news-description">{article.description}</p>
+                            <a href={article.url} className="read-more">Read more</a>
+                        </div>
+                    </div>
+                </li>
+            ))}
+        </ul>
+</div>
+    </div>
+  );
 }
--- app/main-page/page.js
+++ app/main-page/page.js
@@ -1,10 +1,11 @@
 import Header from "./header";
+import Articles from "../api/newsapi";
 
 export default function MainPage(){
     return (
         <div className="main-page">
             <Header></Header>
-
+            <Articles></Articles>
         </div>
     );
 }
