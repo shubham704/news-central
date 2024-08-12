9239091f40dcbbaba30cc9946aeb4412586ad117
shubham704 <shubhamprasharr40@gmail.com>
2024-08-12 17:44:04 -0600

    Newscentral folder removed

--- newscentral/app/index.js
+++
@@ -1,5 +0,0 @@
-import MainPage from './main-page/page';
-
-export default function Home() {
-  return <MainPage />;
-}
--- newscentral/app/main-page/layout.js
+++
@@ -1,8 +0,0 @@
-import { AuthContextProvider } from "../login-page/_utils/auth-context";
- 
-const Layout = ({ children }) => {
-  return <AuthContextProvider>{children}</AuthContextProvider>;
-};
- 
-export default Layout;
- 
