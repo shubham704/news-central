6651abe1b25bbde46a461906d6f47dc969e9ebbd
shubham704 <shubhamprasharr40@gmail.com>
2024-08-12 18:52:26 -0600

    added multiple apis to handle null response

--- app/api/newsapi.js
+++ app/api/newsapi.js
@@ -8,17 +8,30 @@ import { useState, useEffect } from "react";
 
 export default function Articles({query}) {
   const [articles, setArticles] = useState([]);
-
-  const fetchArticles = async (url) => {
+  const apiUrls = [
+    '929064942e7a451f89b6cbb8c86f1ad1',
+    'bc205442472a4a35afe50184f0e45751',
+    '2947e985498b4dfaa67362cbae119282',
+    '6be924e3a0a84415925f0d40380e8ecd',
+    '8328ac1d24ce493bb3570aa8786118ee'
+  ];
+  const fetchArticles = async () => {
+    for (let api of apiUrls) {
     try {
       const response = await fetch(
-        `https://newsapi.org/v2/everything?q=${query}&language=en&sortBy=relevancy&apiKey=bc205442472a4a35afe50184f0e45751`
+        `https://newsapi.org/v2/everything?q=${query}&language=en&sortBy=relevancy&apiKey=${api}`
       );
+      if (!response.ok) {
+        console.log("API failed, trying next API");
+        continue;
+      }
       const articleData = await response.json();
       return articleData.articles
     } catch (error) {
       console.error("Error fetching articles:", error);
     }
+  }
+  console.log("All APIs failed");
   };
 
  
